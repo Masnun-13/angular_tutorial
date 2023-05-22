4afd8120e2dd02232220d444dabb92828fe3e24a
Masnun-13 <masnun111112@gmail.com>
2023-05-22 16:00:51 +0600

    around 4 hour mark of tutorial

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'hinv-rooms',
@@ -13,6 +14,45 @@ export class RoomsComponent implements OnInit {
   numberOfRooms = 10;
 
   hideRooms = false
+
+  rooms : Room = {
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms : 5,
+  };
+
+  roomList: RoomList[] = [
+    {
+      roomNumber: 1,
+      roomType: "Deluxe Room",
+      amenities: "Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen",
+      price: 500,
+      photos:
+        "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixl",
+      checkInTime: new Date('11-Nov-2021'),
+      checkOutTime: new Date('12-Nov-2021'),
+    },
+    {
+      roomNumber: 2,
+      roomType: "Deluxe Room",
+      amenities: "Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen",
+      price: 1000,
+      photos:
+        "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixl",
+      checkInTime: new Date('11-Nov-2021'),
+      checkOutTime: new Date('12-Nov-2021'),
+    },
+    {
+      roomNumber: 3,
+      roomType: "Private Suite",
+      amenities: "Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen",
+      price: 15000,
+      photos:
+        "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixl",
+      checkInTime: new Date('11-Nov-2021'),
+      checkOutTime: new Date('12-Nov-2021'),
+    }
+  ]
 
   toggle(){
     this.hideRooms = !this.hideRooms
