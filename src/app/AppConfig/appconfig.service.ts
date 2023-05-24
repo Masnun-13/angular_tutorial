f27f14f6880a6ed172690d4d2c9a0cf8c063f47c
Masnun-13 <masnun111112@gmail.com>
2023-05-24 15:12:54 +0600

    environments 5

--- src/app/AppConfig/appconfig.service.ts
+++ src/app/AppConfig/appconfig.service.ts
@@ -3,3 +3,7 @@ import { InjectionToken } from '@angular/core'
 import { AppConfig } from "./appconfig.interface";
 
 export const APP_CONFIG_SERVICE = new InjectionToken<AppConfig>('app config')
+
+export const APP_CONFIG : AppConfig = {
+  apiEndPoint : environment.apiEndPoint
+}
