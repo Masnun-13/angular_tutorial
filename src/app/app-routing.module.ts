290ec01dc9270d47367de8712548962efd656a78
Masnun-13 <masnun111112@gmail.com>
2023-05-28 16:05:41 +0600

    forms work, but not crud (that works in crud branch)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
 import { RoomsComponent } from './rooms/rooms.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { RoomsBookingComponent } from './rooms/rooms-booking/rooms-booking.component';
+import { RoomsAddComponent } from './rooms/rooms-add/rooms-add.component';
 
 const routes: Routes = [
   {path: 'employee', component: EmployeeComponent},
   {path: 'rooms', component: RoomsComponent},
-  {path: '', redirectTo: '/rooms', pathMatch: 'full'}
+  {path: 'rooms/add', component: RoomsAddComponent},
+  {path: 'rooms/:roomid', component: RoomsBookingComponent},
+  {path: '', redirectTo: '/rooms', pathMatch: 'full'},
+  {path: '**', component: NotfoundComponent}
 ];
 
 @NgModule({
---
+++ src/app/notfound/notfound.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'hinv-notfound',
+  templateUrl: './notfound.component.html',
+  styleUrls: ['./notfound.component.scss']
+})
+export class NotfoundComponent implements OnInit {
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+
+
+  }
+
+}
